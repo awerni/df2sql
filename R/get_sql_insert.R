89496fccf7ea64b4c9eebfb8c59fc34b2f92b122
Andreas Wernitznig <andreas@wernitznig.com>
2018-12-06 23:03:08 +0100

    inital import

---
+++ R/get_sql_insert.R
@@ -0,0 +1,19 @@
+get_sql_insert <- function(df, tablename) {
+  type_df <- map_df(df, class) %>% 
+    gather() %>% 
+    mutate(sep = ifelse(value == "character", "'", "")) %>%
+    select(-value)
+  
+  df %>%
+    mutate(sql_id = 1:n()) %>%
+    gather(key, value, -sql_id) %>%
+    full_join(type_df, by = "key") %>%
+    mutate(sql = ifelse(is.na(value), paste0("NULL"), 
+                        paste0(sep, value, sep))) %>%
+    select(-value, -sep) %>%
+    spread(key, sql)  %>%
+    mutate(sql_insert = paste0("INSERT INTO ", tablename, " (", paste(colnames(df), collapse = ", "), ")")) %>%
+    unite(sql_col, colnames(df), sep = ",", remove = TRUE) %>%
+    mutate(sql = paste0(sql_insert, " VALUES (", sql_col, ")")) %>%
+    select(sql)
+}
---
+++ R/get_sql_upsert.R
@@ -0,0 +1,59 @@
+get_sql_upsert <- function(new_df, old_df, key_col, tablename, del_old = FALSE) {
+
+  if (any(sort(colnames(new_df)) != sort(colnames(old_df)))) stop("column names are different")
+  
+  type_old_df <- map_df(old_df, class) %>% gather()
+  type_new_df <- map_df(new_df, class) %>% gather()
+  
+  coltypes_different <- type_old_df %>% 
+    left_join(type_new_df, by = "key") %>% 
+    mutate(identical = value.x != value.y) %>% 
+    .$identical %>% any()
+  
+  if (coltypes_different) stop("column types are different")
+  
+  val_col <- setdiff(colnames(new_df), key_col)
+  
+  keys_from_old <- old_df %>% select_(key_col)
+  keys_from_new <- new_df %>% select_(key_col)
+  
+  # --- new in new_df
+  new_keys <- anti_join(keys_from_new, keys_from_old, by = key_col)
+  new_insert_df <- new_keys %>% inner_join(new_df, by = key_col)
+  new_overlap_df <- anti_join(new_df, new_keys, by = key_col)
+    
+  result_insert <- get_sql_insert(new_insert_df, tablename) %>% select(sql)
+  
+  # --- discarded in new_df
+  old_keys <- anti_join(keys_from_old, keys_from_new, by = key_col)
+  old_clean_df <- old_df %>% anti_join(old_keys, by = key_col)
+  
+  result_delete <- NULL
+  if (del_old & nrow(old_keys) > 0) {
+    result_delete <- get_sql_delete(old_keys, tablename)
+  }
+  
+  # --- present in both 
+  # check for changes
+  
+  old_df1 <- old_clean_df %>%
+    gather_("key", "value", gather_cols = val_col)
+  
+  new_df1 <- new_overlap_df %>%
+    gather_("key", "value", gather_cols = val_col)
+  
+  class_def <- new_overlap_df %>% map_df(class) %>% map(as.character)
+  
+  # records with new values
+  result_update <- old_df1 %>% 
+    inner_join(new_df1, by = c(key_col, "key")) %>%
+    filter(value.x != value.y | is.na(value.x) != is.na(value.y)) %>%
+    select(-value.x) %>%
+    spread(key, value.y) %>%
+    type_convert() %>%
+    bind_cols(get_sql_update(., key_col, tablename)) %>%
+    select(sql)
+  
+  bind_rows(result_update, result_insert, result_delete)
+  
+}
